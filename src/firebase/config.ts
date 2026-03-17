b512e9a377b66f8d60b054d36dd37d008bb668aa
Atul Gangurde <atulgangurde3002@gmail.com>
2026-03-17 23:12:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7637896452-383c8",
+  "appId": "1:694519597359:web:2d519381db75a1ed0df9da",
+  "apiKey": "AIzaSyCmZBJikdSg1_3ElniG5p54G01-4R-T6jU",
+  "authDomain": "studio-7637896452-383c8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "694519597359"
+};
